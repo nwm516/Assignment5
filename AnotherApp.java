b380bc6dc4d16e75283fecd014c95fe1bd2de80a
NW Miller <nwm.516@gmail.com>
2023-11-09 20:05:30 -0800

    First commit from AD400

---
+++ AnotherApp.java
@@ -0,0 +1,7 @@
+public class AnotherApp {
+
+    public static void main (String[] args) {
+        System.out.println("How many more apps will he write???");
+		System.out.println("We may never truly know...");
+    }
+}
---
+++ MyFirstApp.java
@@ -0,0 +1,7 @@
+public class MyFirstApp {
+
+    public static void main (String[] args) {
+        System.out.println("Hello worldy world.");
+		System.out.println("What a lovely day it is!");
+    }
+}
